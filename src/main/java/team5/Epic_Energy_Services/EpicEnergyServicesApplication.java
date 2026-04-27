622d5fd3731ce98de70db4b6659eaef5034c2bf7
Giulia Creps <giulia.creps2@gmail.com>
2026-04-27 10:25:14 +0200

    Add initial Spring Boot project scaffold

---
+++ src/main/java/team5/Epic_Energy_Services/EpicEnergyServicesApplication.java
@@ -0,0 +1,13 @@
+package team5.Epic_Energy_Services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicEnergyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicEnergyServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team5/Epic_Energy_Services/EpicEnergyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package team5.Epic_Energy_Services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
