281b0be8be0e8b0d6d957cfcca319eb342e25e77
Giulia Creps <giulia.creps2@gmail.com>
2026-04-27 23:27:10 +0200

    Move UserRoleService to services package

--- src/main/java/team5/Epic_Energy_Services/services/UserRoleService.java
+++ src/main/java/team5/Epic_Energy_Services/services/UserRoleService.java
@@ -1,22 +1,26 @@
-package team5.Epic_Energy_Services;
+package team5.Epic_Energy_Services.services;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import team5.Epic_Energy_Services.entities.UserRole;
+import team5.Epic_Energy_Services.repositories.UserRoleRepository;
 
 @Service
 public class UserRoleService {
     private final UserRoleRepository userRoleRepository;
+
     public UserRoleService(UserRoleRepository userRoleRepository) {
         this.userRoleRepository = userRoleRepository;
     }
-    public UserRole save(UserRole userRole){
-         return this.userRoleRepository.save(userRole);
+
+    public UserRole save(UserRole userRole) {
+        return this.userRoleRepository.save(userRole);
     }
-    public Page<UserRole> findAll(int page,int size,String sortBy){
-        Pageable pageable = PageRequest.of(page,size);
+
+    public Page<UserRole> findAll(int page, int size, String sortBy) {
+        Pageable pageable = PageRequest.of(page, size);
         return this.userRoleRepository.findAll(pageable);
     }
 }
