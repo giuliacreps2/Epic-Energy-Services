723a888b4421bebdc63f7676e6a92a647884732b
Giulia Creps <giulia.creps2@gmail.com>
2026-04-29 12:52:54 +0200

    Add B2bClient Specifications

--- src/main/java/team5/Epic_Energy_Services/repositories/ClientsRepository.java
+++ src/main/java/team5/Epic_Energy_Services/repositories/ClientsRepository.java
@@ -3,6 +3,7 @@ package team5.Epic_Energy_Services.repositories;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import team5.Epic_Energy_Services.entities.B2bClient;
 
@@ -11,7 +12,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface ClientsRepository extends JpaRepository<B2bClient, UUID> {
+public interface ClientsRepository extends JpaRepository<B2bClient, UUID>, JpaSpecificationExecutor<B2bClient> {
     //FILTRI
     //Data inizio e fine contatto
     Page<B2bClient> findAllByCreatedAt(LocalDate createdAt, Pageable pageable);
